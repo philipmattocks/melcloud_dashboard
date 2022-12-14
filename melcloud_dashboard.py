530454b7fa9d3664396cfb52697d9893a4f89dbd
Philip Mattocks <philipmattocks@Philips-Air.lan>
2022-12-14 19:37:19 +0000

    changed names of axes and stats

--- melcloud_dashboard.py
+++ melcloud_dashboard.py
@@ -9,7 +9,7 @@ import json
 from io import BytesIO
 from datetime import timedelta
 from datetime import datetime
-
+#
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 # df = pd.read_csv('./CSVReport_7Dec_short.csv')
 
@@ -47,11 +47,11 @@ print(f"Getting data from MELCloud...")
 response_get_data = s.get(get_data_url)
 print(f"Processing data...")
 df = pd.read_csv(BytesIO(response_get_data.content))
-df["date"] = pd.to_datetime(df["TimeStamp"], format='%d/%m/%Y %H:%M')
-df["year"] = df["date"].dt.year
+df["date/time"] = pd.to_datetime(df["TimeStamp"], format='%d/%m/%Y %H:%M')
+df["year"] = df["date/time"].dt.year
 df = df[
     [
-        "date",
+        "date/time",
         "year",
         "RoomTemperatureZone1",
         "RoomTemperatureZone2",
@@ -61,20 +61,20 @@ df = df[
         "EnergyProduced",
     ]
 ]
-df_melted = df.melt(id_vars=["date", "year"], var_name="measurement", value_name="temp")
+df_melted = df.melt(id_vars=["date/time", "year"], var_name="measurement", value_name="temp/energy")
 
 app = Dash(__name__)
 app.layout = html.Div(
     [
         dcc.DatePickerRange(
             id="my-date-picker-range",
-            min_date_allowed=min(df["date"]),
-            max_date_allowed=max(df["date"]),
+            min_date_allowed=min(df["date/time"]),
+            max_date_allowed=max(df["date/time"]),
             end_date=datetime.now().date() + timedelta(days=1),
             start_date=datetime.now().date() - timedelta(days=1)
             # initial_visible_month=date(2017, 8, 5),
-            # start_date=min(df['date']),
-            # end_date=max(df['date'])
+            # start_date=min(df['date/time']),
+            # end_date=max(df['date/time'])
         ),
         dcc.Graph(id="graph"),
         html.H3(id="output-container-date-picker-range"),
@@ -95,27 +95,27 @@ app.layout = html.Div(
     Input("my-date-picker-range", "end_date"),
 )
 def update_output(start_date, end_date):
-    mask = (df_melted["date"] > datetime.strptime(start_date, '%Y-%m-%d')) & (df_melted["date"] <= datetime.strptime(end_date,'%Y-%m-%d'))
+    mask = (df_melted["date/time"] > datetime.strptime(start_date, '%Y-%m-%d')) & (df_melted["date/time"] <= datetime.strptime(end_date,'%Y-%m-%d'))
     filtered_df = df_melted.loc[mask]
-    fig = px.line(filtered_df, x="date", y="temp", color="measurement")
+    fig = px.line(filtered_df, x="date/time", y="temp/energy", color="measurement")
     fig.update_layout(transition_duration=500)
     mean_outside_temp = round(
         filtered_df.loc[filtered_df["measurement"] == "OutsideTemperature"][
-            "temp"
+            "temp/energy"
         ].mean(),
         2,
     )
     total_energy_produced = filtered_df.loc[
         filtered_df["measurement"] == "EnergyProduced"
-    ]["temp"].sum()
+    ]["temp/energy"].sum()
     total_energy_consumed = filtered_df.loc[
         filtered_df["measurement"] == "EnergyConsumed"
-    ]["temp"].sum()
+    ]["temp/energy"].sum()
     return (
         f"mean outside temp: {mean_outside_temp}",
-        f"POC: {round(total_energy_produced/total_energy_consumed, 2)}",
-        f"Total Energy Produced: {total_energy_produced}",
-        f"Total Energy Consumed: {total_energy_consumed}",
+        f"kWh Produced: {total_energy_produced/1000}",
+        f"kWh Consumed: {total_energy_consumed/1000}",
+        f"COP: {round(total_energy_produced / total_energy_consumed, 2)}",
         fig,
     )
 
